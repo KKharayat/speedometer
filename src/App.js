b14de2f01406d6c13e4b5b394db9aeb224663c66
KKharayat <keertikharayat@gmail.com>
2021-02-11 16:09:41 +0530

    Speedometer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import lines from "./images/lines.png";
+import number from "./images/num.png";
+
+let interval = null;
+let r = 255;
+let cf = 2 * Math.PI * r;
+
+class App extends Component {
+  state = { speed: 0, length: 0 };
+
+  timer = () => {
+    this.setState({
+      speed: this.state.speed + 1,
+      length: this.state.length + 5.02,
+    });
+
+    if (this.state.speed === 220) {
+      clearInterval(interval);
+    }
+  };
+
+  componentDidMount() {
+    interval = setInterval(this.timer, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(interval);
+  }
+  render() {
+    {
+      return (
+        <div className="root">
+          <svg className="meter" xmlns="http://www.w3.org/2000/svg">
+            <circle
+              xmlns="http://www.w3.org/2000/svg"
+              className="circle width"
+              stroke="#EE9A34"
+              r="255"
+              id="svg_4"
+              cy="50%"
+              cx="50%"
+              style={{
+                strokeDasharray: this.state.length + "," + cf,
+              }}
+            ></circle>
+            <circle
+              className="circle width"
+              cx="50%"
+              cy="50%"
+              stroke="#7CCCE5"
+              r="255"
+              style={{ strokeDasharray: 0 + "," + cf }}
+            ></circle>
+          </svg>
+          <div className="speed_count">
+            <label className="speed_val">{this.state.speed}</label>
+            <label className="unit">kmh</label>
+          </div>
+          <img src={lines} alt="lines" className="lines" />
+          <img src={number} alt="numbers" className="number" />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
